7f1051307291c15a7f0303967524c7a91a48a341
naziradidier <nazirap20.aps1b@gmai.com>
2021-02-27 06:58:47 +0100

    lien href sur tableau dimension

--- pageToPage.html.php
+++ pageToPage.html.php
@@ -48,46 +48,46 @@
 
   <!--****************************************************************************************** -->
   <?php
-//   boucler un tableau avec un lien
-  // $table = array("Nazirah", "Miora", "Toavina");
-  // echo "<ul>";
-  // for ($i = 0; $i < count($table); $i++) {
-  //   echo "<li>";
-  //   echo "<a href=\"http://localhost/test/$table[$i].html.php\"> $table[$i]</a>";
-  //   echo "</li>";
-  // }
-  // echo "</ul>";
-  // ?>
+  boucler un tableau avec un lien
+  $table = array("Nazirah", "Miora", "Toavina");
+  echo "<ul>";
+  for ($i = 0; $i < count($table); $i++) {
+    echo "<li>";
+    echo "<a href=\"http://localhost/test/$table[$i].html.php\"> $table[$i]</a>";
+    echo "</li>";
+  }
+  echo "</ul>";
+  ?>
  <!--****************************************************************************************** -->
 <?php
-// $Personne = array (
-//     $colonne = array("Nom", "Nazirah", "Miora", "Toavina"),
-//     $colonne = array("Prénom","Prisca", "Jelisca", "Mbola"),
-//     $colonne = array("age",19, 20, 21),
-//     $colonne = array("Lieu","Ambanja", "Diego", "Majunga")
-// );
+$Personne = array (
+    $colonne = array("Nom", "Nazirah", "Miora", "Toavina"),
+    $colonne = array("Prénom","Prisca", "Jelisca", "Mbola"),
+    $colonne = array("age",19, 20, 21),
+    $colonne = array("Lieu","Ambanja", "Diego", "Majunga")
+);
 
-// // afficher miora seulement
-// for ($ligne= 0; $ligne < count($colonne); $ligne++) {
-//   echo "<dl>";
-//   for ($col = 0; $col < count($colonne); $col+=3) {
-//     echo "<dd>".$Personne[$ligne][$col]."</dd>";
-//   }
-//   echo "</dl>";
-// }
-    // echo $Personne[0][0].": ".$Personne[0][2].".<br>";
-    // echo $Personne[1][0]." :".$Personne[1][2].".<br>";
-    // echo $Personne[2][0].":".$Personne[2][2].".<br>";
-    // echo $Personne[3][0].": ".$Personne[3][2].".<br>";
-    // *****************************************************
-    // afficher miora seulement
-    // for ($ligne= 0; $ligne < count($colonne); $ligne++) {
-    //   echo "<dl>";
-    //   for ($col = 0; $col < count($colonne)-1; $col+=2) {
-    //     echo "<dd>".$Personne[$ligne][$col]."</dd>";
-    //   }
-    //   echo "</dl>";
-    // }
+// afficher miora seulement
+for ($ligne= 0; $ligne < count($colonne); $ligne++) {
+  echo "<dl>";
+  for ($col = 0; $col < count($colonne); $col+=3) {
+    echo "<dd>".$Personne[$ligne][$col]."</dd>";
+  }
+  echo "</dl>";
+}
+    echo $Personne[0][0].": ".$Personne[0][2].".<br>";
+    echo $Personne[1][0]." :".$Personne[1][2].".<br>";
+    echo $Personne[2][0].":".$Personne[2][2].".<br>";
+    echo $Personne[3][0].": ".$Personne[3][2].".<br>";
+    *****************************************************
+    afficher miora seulement
+    for ($ligne= 0; $ligne < count($colonne); $ligne++) {
+      echo "<dl>";
+      for ($col = 0; $col < count($colonne)-1; $col+=2) {
+        echo "<dd>".$Personne[$ligne][$col]."</dd>";
+      }
+      echo "</dl>";
+    }
     // *****************************************************
  ?>  
 
